973a287a6c2b2ef92b3b18db04e5386871ba1b92
Nathan Castle <ncastle@esri.com>
2022-09-15 13:13:10 -0700

    Add files via upload

---
+++ ARCoreBuildTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ARCoreBuildTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
